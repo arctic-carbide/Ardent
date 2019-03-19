f17d42c5845f03fde6e0f749b99bf693172a737a
Mitchell Myers <myersmk@umich.edu>
2019-03-19 11:31:35 -0400

    trying to figure out how the AI behavior tree execution flow works; seems like it's got a special way of executing actions that isn't straightforward

---
+++ Source/Ardent/Public/NewMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/MovementComponent.h"
+#include "NewMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARDENT_API UNewMovementComponent : public UMovementComponent
+{
+	GENERATED_BODY()
+	
+};
